9d32de6440c724802abdf51c3a8262426bb003f2
kkqy <kuokongqingyun@gmail.com>
2022-10-08 03:52:22 +0800

    Add test

--- syncmap.go
+++ syncmap.go
@@ -1,12 +1,14 @@
 package syncmap
 
-import "sync"
+import (
+	"sync"
+)
 
 type SyncMap[KeyT, ValueT any] struct {
 	syncMap sync.Map
 }
 
-func (p SyncMap[KeyT, ValueT]) Load(key KeyT) (ValueT, bool) {
+func (p *SyncMap[KeyT, ValueT]) Load(key KeyT) (ValueT, bool) {
 	value, ok := p.syncMap.Load(key)
 	if ok {
 		return value.(ValueT), ok
@@ -16,15 +18,15 @@ func (p SyncMap[KeyT, ValueT]) Load(key KeyT) (ValueT, bool) {
 	}
 
 }
-func (p SyncMap[KeyT, ValueT]) Store(key KeyT, value ValueT) {
+func (p *SyncMap[KeyT, ValueT]) Store(key KeyT, value ValueT) {
 	p.syncMap.Store(key, value)
 }
-func (p SyncMap[KeyT, ValueT]) LoadOrStore(key KeyT, value ValueT) (ValueT, bool) {
+func (p *SyncMap[KeyT, ValueT]) LoadOrStore(key KeyT, value ValueT) (ValueT, bool) {
 	actual, loaded := p.syncMap.LoadOrStore(key, value)
 	return actual.(ValueT), loaded
 }
 
-func (p SyncMap[KeyT, ValueT]) LoadAndDelete(key KeyT) (ValueT, bool) {
+func (p *SyncMap[KeyT, ValueT]) LoadAndDelete(key KeyT) (ValueT, bool) {
 	value, ok := p.syncMap.LoadAndDelete(key)
 	if ok {
 		return value.(ValueT), ok
@@ -34,10 +36,10 @@ func (p SyncMap[KeyT, ValueT]) LoadAndDelete(key KeyT) (ValueT, bool) {
 	}
 }
 
-func (p SyncMap[KeyT, ValueT]) Delete(key KeyT) {
+func (p *SyncMap[KeyT, ValueT]) Delete(key KeyT) {
 	p.syncMap.Delete(key)
 }
-func (p SyncMap[KeyT, ValueT]) Range(f func(key KeyT, value ValueT) bool) {
+func (p *SyncMap[KeyT, ValueT]) Range(f func(key KeyT, value ValueT) bool) {
 	p.syncMap.Range(func(key, value any) bool {
 		return f(key.(KeyT), value.(ValueT))
 	})
